a83eadf54cfa78b905202c7f6ee5e67909f868d1
Disha Singh <dishasingghh@gmail.com>
2026-06-04 13:19:55 +0530

    rest

--- barchart.py
+++ barchart.py
@@ -1,13 +1,7 @@
 import matplotlib as mpl;
 import matplotlib.pyplot as plt;
 import numpy as np;
-categories=np.array(["Grains","Vegetables","Fruits","Dairy","Meat"]);
-values=np.array([20,15,30,10,25]);
-#bar chart
-# plt.bar(categories, values, color=['red', 'green', 'blue', 'orange', 'purple']);
-# plt.xlabel("Food Categories");
-# plt.ylabel("Values");
-# plt.title("Bar Chart");
-#pie chart 
-plt.pie(values, labels=categories, autopct='%1.1f%%', explode=(0, 0, 0, 0, 0.1), colors=['red', 'green', 'blue', 'orange', 'purple']);
+scores=np.random.normal(loc=80, scale=10, size=100);
+plt.hist(scores);
 plt.show();
+         
